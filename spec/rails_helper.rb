5aacdfb0a7fb7fbc1eeec3441d600a8208e0f193
João Guioto <johnguioto@gmail.com>
2023-09-03 16:11:19 -0300

    Configurando VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
+require 'vcr'
+
 ENV['RAILS_ENV'] ||= 'test'
+
 require_relative '../config/environment'
+
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
@@ -18,13 +22,18 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
-  
+
   config.filter_rails_from_backtrace!
 end
 
-  Shoulda::Matchers.configure do |config|
+Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
       with.library :rails
     end
-  end
+end
+
+VCR.configure do |config|
+    config.cassette_library_dir = 'fixtures/vcr'
+    config.hook_into :webmock
+end
