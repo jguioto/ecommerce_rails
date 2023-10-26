bb7a135dea285d807988b4df750d7f2cd4789f4d
João Guioto <johnguioto@gmail.com>
2023-10-26 20:36:02 -0300

    Gerando as views do Devise

--- db/migrate/20231026034946_devise_create_users.rb
+++ db/migrate/20231026034946_devise_create_users.rb
@@ -14,6 +14,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Rememberable
       t.datetime :remember_created_at
 
+      t.string :name, null: false
+      t.string :phone, null: false
+      
+      #Ex:- :null => false
+      
+
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
